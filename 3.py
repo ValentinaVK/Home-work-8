579c0d20e34e3a716555321d2d383c44ec8d4713
ValentinaVK <Klepikova.V.V@yandex.ru>
2022-01-26 09:09:52 +0300

    home work 8

---
+++ 3.py
@@ -0,0 +1,20 @@
+from functools import wraps
+
+def type_logger(level=0):
+     def logger(func):
+        @wraps(func)
+        def decor(*argv):
+            if level >> 0:
+                return 'calc_cube(' + ", ".join([f"{func(x)}:{type(func(x))}" for x in argv]) + ")"
+
+            else:
+                return "calc_cube(" + ", ".join([str(func(x)) for x in argv]) + ")"
+
+        return decor
+
+    return logger
+
+@type_logger(2)
+
+def calc_cube(x):
+    return x ** 3
